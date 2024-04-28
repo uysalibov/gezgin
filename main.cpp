19be0c7307ef08dcb31d43980a0523ccb925c965
İbrahim Uysal <uysalibov@gmail.com>
2024-04-28 18:34:55 +0300

    update: read test data from given path

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "renderer.h"
+#include "OSMJson.h"
 
 int main()
 {
     gez::renderer::init(1080, 720, "Gezgin Pathfinding Visualizer");
+
+    gez::fileparser::ReadFile("./Data/simpleIST.json");
+    // gez::fileparser::PrintData();
 
     gez::renderer::run();
     return 0;
