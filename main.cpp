4c58acb31b4f9b040c2900e117938bdaead52a91
İbrahim Uysal <uysalibov@gmail.com>
2024-04-29 16:33:06 +0300

    update: test with tiny data (contains 2 way)

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@ int main()
 {
     gez::renderer::init(1080, 720, "Gezgin Pathfinding Visualizer");
 
-    gez::fileparser::ReadFile("./Maps/simpleIST.json");
-    // gez::fileparser::PrintData();
+    gez::fileparser::ReadFile("./Maps/simpleIST2.json");
+    gez::fileparser::ParseData();
+    gez::fileparser::StandardizeData();
+    gez::fileparser::ScaleData(100, 500);
+    gez::fileparser::PrintData();
 
     gez::renderer::run();
     return 0;
