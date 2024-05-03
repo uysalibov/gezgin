b2d803501b011c832e334ac3206a9c3d91f9ac57
İbrahim Uysal <uysalibov@gmail.com>
2024-05-03 18:56:57 +0300

    feat:  add new points to adj list as a hashed

--- FileParser/include/OSMJson.h
+++ FileParser/include/OSMJson.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <map>
 #include "nlohmann/json.hpp"
 #include "raylib.h"
 #include "KeyTypes.h"
@@ -12,10 +13,15 @@ namespace gez
         extern std::vector<Vector4> lines;
         extern std::vector<gez::HighwayType> highwayTypes;
 
+        extern std::map<size_t, int> nodeMap;
+        extern std::vector<Vector2> nodes;
+
         void ReadFile(const char *path);
         void PrintData();
+        void AddNode(Vector2 node);
         void ParseData();
         Vector4 *GetBounds();
+        size_t GetHash(Vector2 node);
         void StandardizeData();
         void ScaleData(int scaleFactor);
     }
--- FileParser/src/OSMJson.cpp
+++ FileParser/src/OSMJson.cpp
@@ -1,10 +1,14 @@
 #include "OSMJson.h"
+#include "AdjList.h"
 #include <fstream>
 #include <iostream>
 
 nlohmann::json gez::fileparser::jsonData;
 std::vector<Vector4> gez::fileparser::lines;
 std::vector<gez::HighwayType> gez::fileparser::highwayTypes;
+
+std::map<size_t, int> gez::fileparser::nodeMap;
+std::vector<Vector2> gez::fileparser::nodes;
 
 void gez::fileparser::ReadFile(const char *path)
 {
@@ -28,6 +32,18 @@ void gez::fileparser::PrintData()
     }
 }
 
+void gez::fileparser::AddNode(Vector2 node)
+{
+    size_t hash = GetHash(node);
+    std::cout << "Hash: " << hash << std::endl;
+
+    if (nodeMap.find(hash) == nodeMap.end())
+    {
+        nodes.push_back(node);
+        nodeMap[hash] = nodes.size() - 1;
+    }
+}
+
 void gez::fileparser::ParseData()
 {
     for (auto &element : jsonData["elements"])
@@ -36,66 +52,89 @@ void gez::fileparser::ParseData()
         {
             Vector4 bounds = {element["bounds"]["minlat"], element["bounds"]["minlon"], element["bounds"]["maxlat"], element["bounds"]["maxlon"]};
             int size = element["geometry"].size();
-            for (int i = 0; i < size - 1; i++)
+            for (int i = 0; i < size; i++)
             {
-                Vector4 line;
-                line.x = element["geometry"][i]["lat"].get<double>();
-                line.y = element["geometry"][i]["lon"].get<double>();
-                line.z = element["geometry"][i + 1]["lat"].get<double>();
-                line.w = element["geometry"][i + 1]["lon"].get<double>();
-                lines.push_back(line);
-                highwayTypes.push_back(HighwayType::UNCLASSIFIED);
+                Vector2 node;
+                node.x = element["geometry"][i]["lat"].get<float>();
+                node.y = element["geometry"][i]["lon"].get<float>();
+
+                AddNode(node);
             }
         }
     }
+
+    // add adjacencies
+    TraceLog(LOG_INFO, "Initializing adjacency list");
+    gez::AdjList::Init(nodes.size());
+    TraceLog(LOG_INFO, "Adding adjacencies");
+    for (auto &element : jsonData["elements"])
+    {
+        if (element["type"] == "way")
+        {
+            int size = element["geometry"].size();
+            for (int i = 0; i < size - 1; i++)
+            {
+                Vector2 node1;
+                node1.x = element["geometry"][i]["lat"].get<float>();
+                node1.y = element["geometry"][i]["lon"].get<float>();
+
+                Vector2 node2;
+                node2.x = element["geometry"][i + 1]["lat"].get<float>();
+                node2.y = element["geometry"][i + 1]["lon"].get<float>();
+
+                int pos1 = nodeMap[GetHash(node1)];
+                int pos2 = nodeMap[GetHash(node2)];
+
+                gez::AdjList::AddAdj(pos1, pos2);
+                gez::AdjList::AddAdj(pos2, pos1);
+            }
+        }
+    }
+    TraceLog(LOG_INFO, "Adjacencies added");
 }
 
 Vector4 *gez::fileparser::GetBounds()
 {
-    Vector4 *bounds = new Vector4{lines[0].x, lines[0].y, lines[0].z, lines[0].w};
-    for (int i = 1, size = lines.size(); i < size; i++)
+    float max = std::numeric_limits<float>::max();
+    float min = std::numeric_limits<float>::min();
+    Vector4 *bounds = new Vector4{max, max, min, min};
+    for (int i = 1, size = nodes.size(); i < size; i++)
     {
-        Vector4 line = lines[i];
-        if (line.x < bounds->x)
-            bounds->x = line.x;
-        if (line.y < bounds->y)
-            bounds->y = line.y;
-        if (line.z > bounds->z)
-            bounds->z = line.z;
-        if (line.w > bounds->w)
-            bounds->w = line.w;
+        if (nodes[i].x < bounds->x)
+            bounds->x = nodes[i].x;
+        if (nodes[i].y < bounds->y)
+            bounds->y = nodes[i].y;
+        if (nodes[i].x > bounds->z)
+            bounds->z = nodes[i].x;
+        if (nodes[i].y > bounds->w)
+            bounds->w = nodes[i].y;
     }
     return bounds;
+}
+
+size_t gez::fileparser::GetHash(Vector2 node)
+{
+    std::string x = std::to_string(node.x);
+    std::string y = std::to_string(node.y);
+    return std::hash<std::string>{}(x + y);
 }
 
 void gez::fileparser::StandardizeData()
 {
     Vector4 *bounds = GetBounds();
-    for (int i = 0, size = lines.size(); i < size; i++)
+    for (int i = 0, size = nodes.size(); i < size; i++)
     {
-        Vector4 line = lines[i];
-        line.x = (line.x - bounds->x) / (bounds->z - bounds->x);
-        line.y = (line.y - bounds->y) / (bounds->w - bounds->y);
-        line.z = (line.z - bounds->x) / (bounds->z - bounds->x);
-        line.w = (line.w - bounds->y) / (bounds->w - bounds->y);
-
-        lines[i] = line;
+        nodes[i].x = (nodes[i].x - bounds->x) / (bounds->z - bounds->x);
+        nodes[i].y = (nodes[i].y - bounds->y) / (bounds->w - bounds->y);
     }
     std::cout << "Bounds: " << bounds->x << " " << bounds->y << " - " << bounds->z << " " << bounds->w << std::endl;
 }
 
 void gez::fileparser::ScaleData(int scaleFactor)
 {
-    std::cout << "XDiff: " << lines[0].z - lines[0].x << " YDiff: " << lines[0].w - lines[0].y << std::endl;
-    float multiplier = (lines[0].z - lines[0].x) / (lines[0].w - lines[0].y);
-    for (int i = 0, size = lines.size(); i < size; i++)
+    for (int i = 0, size = nodes.size(); i < size; i++)
     {
-        Vector4 line = lines[i];
-        line.x *= scaleFactor / multiplier;
-        line.y *= scaleFactor;
-        line.z *= scaleFactor / multiplier;
-        line.w *= scaleFactor;
-
-        lines[i] = line;
+        nodes[i].x *= scaleFactor;
+        nodes[i].y *= scaleFactor;
     }
 }
